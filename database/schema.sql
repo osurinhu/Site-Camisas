5d9cd47bdaaddefc015c45af6d358f8a3280788b
osurinhu <antoniodepaduarabelo767@gmail.com>
2024-05-14 16:44:14 -0300

    Coloquei a database em portugues

--- database/schema.sql
+++ database/schema.sql
@@ -1,52 +1,53 @@
 -- TODO: Preciso pensar mais
-CREATE TABLE users (
+CREATE TABLE usuarios (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    phone_number TEXT,
+    usuario TEXT NOT NULL,
+    numero_telefone TEXT,
     email TEXT NOT NULL,
-    pw_hash TEXT NOT NULL,
+    senha_hash TEXT NOT NULL,
 
-    join_date TEXT NOT NULL,
-    last_pw_change TEXT,
-    last_email_change TEXT,
-    last_login TEXT
+    data_entrada TEXT NOT NULL,
+    senha_ultima_mudanca TEXT,
+    email_ultima_mudanca TEXT,
+    ultimo_login TEXT
 )
--- TODO: Preciso pensar mais TODO: vendedor
-CREATE TABLE products (
+
+-- TODO: Preciso pensar mais
+CREATE TABLE estampas (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    product_name TEXT NOT NULL,
-    product_description TEXT,
+    estampa_nome TEXT NOT NULL,
+    estampa_descricao TEXT,
     price REAL NOT NULL,
     discount INTEGER,
 
+	estampa_dono TEXT NOT NULL,
     add_date TEXT NOT NULL
 )
-
+	
 CREATE TABLE tags (
-
-    product_id INTEGER NOT NULL,
+    estampa_id INTEGER NOT NULL,
     tag TEXT NOT NULL,
 
-    FOREIGN KEY(product_id_id) REFERENCES products(id)
+    FOREIGN KEY(estampa_id) REFERENCES estampa_descricao(id)
 
 )
 
-CREATE TABLE favorites (
-    user_id INTEGER NOT NULL,
-    product_id INTEGER NOT NULL,
-    add_date TEXT NOT NULL,
+CREATE TABLE favoritos (
+    usuario_id INTEGER NOT NULL,
+    estampa_id INTEGER NOT NULL,
+    data_adicao TEXT NOT NULL,
 
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(product_id_id) REFERENCES products(id)
+    FOREIGN KEY(usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY(estampa_id) REFERENCES estampas(id)
 )
 
 CREATE TABLE cart (
-    user_id INTEGER NOT NULL,
-    product_id INTEGER NOT NULL,
+    usuario_id INTEGER NOT NULL,
+    estampa_id INTEGER NOT NULL,
     qtd INTEGER NOT NULL,
-    add_date TEXT NOT NULL,
+    data_adicao TEXT NOT NULL,
 
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(product_id_id) REFERENCES products(id)
+    FOREIGN KEY(user_id) REFERENCES usuarios(id),
+    FOREIGN KEY(estampa_id) REFERENCES estampas(id)
 );
 
