bff25b9aa32b20067cbc54403d3855dc4e9d9af7
osurinhu <antoniodepaduarabelo767@gmail.com>
2024-05-29 14:39:28 -0300

    Verificação de inputs retrabalhada

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from ajudante import *
 from dotenv import load_dotenv
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+from itsdangerous import URLSafeTimedSerializer
 import os
 import re
 import sqlite3
@@ -34,56 +35,46 @@ def registrar():
             flash("Informações em falta")
             return redirect("/registrar")
         
-        # Define variavies para checagem da validez dos inputs
-        checagem = {
-            "emailInvalido": False,
-            "senhasDiferente": False,
-            "emailIndisponivel": False
-        }
-
         # Checa se o email é valido
         regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
         if not (re.fullmatch(regex, request.form.get("email"))):
-            checagem["emailInvalido"] = True
-            flash("Email invalido")
+            
+            flash("Email fora do padrão")
+        
+
+        infoValidas = True
 
         # Checa se senhas inseridas são iguais
         if (user_input["senha"] != user_input["confirmSenha"]):
-            checagem["senhasDiferente"] = True
+            infoValidas = False
             flash("Senhas não correspondem")
 
-        if not checagem["emailInvalido"]: # Não conecta na DB caso email seja invalido
-            # Conecta na databse
-            with sqlite3.connect("database/database.db") as con:
-                cur = con.cursor()
+        # Conecta na databse
+        with sqlite3.connect("database/database.db") as con:
+            cur = con.cursor()
 
-                # TODO: mexer com a database
-                try: 
-                    # Checa se o email ja esta em uso
-                    if cur.execute("SELECT 1 FROM usuarios WHERE email = ?", (user_input["email"],)).fetchone():
-                        checagem["emailIndisponivel"] = True
-                        flash("Email já é utilizado por uma conta")
-                    
-                    # Adicionar conta à DB 
-                    if not any(checagem.values()): # Não executa se alguma checagem for verdade
-                        cur.execute("INSERT INTO usuarios (nome, email, senha_hash, data_entrada) VALUES (?, ?, ?, datetime('now', 'localtime'));",
-                                    (user_input["nome"], user_input["email"], generate_password_hash(user_input["senha"])))
-                        con.commit()
+            # TODO: mexer com a database
+            try: 
+                # Checa se o email ja esta em uso
+                if cur.execute("SELECT 1 FROM usuarios WHERE email = ?", (user_input["email"],)).fetchone():
+                    infoValidas = False
+                    flash("Email já é utilizado por uma conta")
+                
+                if not infoValidas: # Caso conta NÃO possa ser criada
+                    return redirect("/registrar")
+                
+                else:# Caso conta POSSA ser criada
+                    # Adicionar conta à DB
+                    cur.execute("INSERT INTO usuarios (nome, email, senha_hash, data_entrada) VALUES (?, ?, ?, datetime('now', 'localtime'));",
+                                (user_input["nome"], user_input["email"], generate_password_hash(user_input["senha"])))
+                    con.commit()
 
-                        
+                    # Conta foi criada TODO:
+                    return "conta pode ser criada"
 
-            
-                finally:
-                    cur.close()
+            finally:
+                cur.close()
         
-        # Conta nao foi criada
-        if any(checagem.values()):
-                return render_template("registrar.html")
-        
-        
-        # Conta foi criada TODO:
-        return "conta pode ser criada"
-    
     
 
 if __name__ == '__main__':
