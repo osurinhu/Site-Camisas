129524177ce7f23dbafbf2d88a95422b99cd0308
osurinhu <antoniodepaduarabelo767@gmail.com>
2024-05-31 20:04:51 -0300

    Rota entrar adicionada

--- Site_Camisas/blueprints/autenticacao/autenticacao.py
+++ Site_Camisas/blueprints/autenticacao/autenticacao.py
@@ -22,14 +22,13 @@ def registrar():
 
         # Checa se algum input foi deixado em branco
         if not all(user_input.values()):
-            flash("Informações em falta")
+            flash("Informações em falta","info")
             return redirect("/registrar")
         
         # Checa se o email é valido
         regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
         if not (re.fullmatch(regex, request.form.get("email"))):
-            
-            flash("Email fora do padrão")
+            flash("Email fora do padrão","info")
         
 
         infoValidas = True
@@ -37,7 +36,7 @@ def registrar():
         # Checa se senhas inseridas são iguais
         if (user_input["senha"] != user_input["confirmSenha"]):
             infoValidas = False
-            flash("Senhas não correspondem")
+            flash("Senhas não correspondem","info")
 
         # Conecta na databse
         with sqlite3.connect("database/database.db") as con:
@@ -48,7 +47,7 @@ def registrar():
                 # Checa se o email ja esta em uso
                 if cur.execute("SELECT 1 FROM usuarios WHERE email = ?", (user_input["email"],)).fetchone():
                     infoValidas = False
-                    flash("Email já é utilizado por uma conta")
+                    flash("Email já é utilizado por uma conta","info")
                 
                 if not infoValidas: # Caso conta NÃO possa ser criada
                     return redirect("/registrar")
@@ -60,9 +59,54 @@ def registrar():
                     con.commit()
 
                     # Conta foi criada TODO:
-                    return "conta pode ser criada"
+                    flash("Conta Criada","message")
+                    return redirect("/entrar")
 
             finally:
                 cur.close()
         
     
+@autenticacao_bp.route("/entrar", methods=["GET","POST"])
+def entrar():
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    else:   
+
+        # Filtra inputs do usuario    
+        user_input = {
+            "email": request.form.get("email"),
+            "senha": request.form.get("senha")
+        }
+
+        # Checa se todos inputs foram preenchidos
+        if not all(user_input.values()):
+            flash("Informações em falta","info")
+            return redirect("/entrar")
+        
+        # Conecta na databse
+        with sqlite3.connect("database/database.db") as con:
+            cur = con.cursor()
+
+            # TODO: mexer com a database
+            try: 
+                query = cur.execute("SELECT id, senha_hash FROM usuarios WHERE email = ?", (user_input["email"],)).fetchall()
+                
+            finally:
+                cur.close()
+        
+        # Checa se há correspondencia de email na DB
+        if len(query) != 1:
+            flash("Email e/ou senha incorretos","info")
+            return redirect("/entrar")
+
+        # Checa se a senha inserida pelo usuario corresponde com a da DB
+        if not check_password_hash(query[0][1], user_input["senha"]):
+            flash("Email e/ou senha incorretos","info")
+            return redirect("/entrar")
+        
+
+        else:
+            session["user_id"] = query[0][0]
+            return "logado"
+
