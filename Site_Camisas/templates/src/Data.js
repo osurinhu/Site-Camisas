2ccd1133620df3345f4e6b4821ab3659fb762a36
GustavoZom <168488714+GustavoZom@users.noreply.github.com>
2024-06-14 00:19:52 -0300

    Data.js atualizado
    Adicionados ids referentes aos itens da loja

--- Site_Camisas/templates/src/Data.js
+++ Site_Camisas/templates/src/Data.js
@@ -1,10 +1,35 @@
 let shopItemsData = [
   {
-    id: "",
-    name: "",
-    price: 0,
-    desc: "",
-    img: "",
+    id: "item1",
+    price: 20,
+  },
+  {
+    id: "item2",
+    price: 21,
+  },
+  {
+    id: "item3",
+    price: 22,
+  },
+  {
+    id: "item4",
+    price: 23,
+  },
+  {
+    id: "item5",
+    price: 24,
+  },
+  {
+    id: "item6",
+    price: 25,
+  },
+  {
+    id: "item7",
+    price: 26,
+  },
+  {
+    id: "item8",
+    price: 27,
   },
   
 ];
